e60944998ee0db2cdced85ba6663a43fdc27894b
Victoran0 <victoranosunkvic@gmail.com>
2024-12-26 01:48:48 +0100

    customer_name field set to be unique

--- api/models.py
+++ api/models.py
@@ -8,11 +8,8 @@ class EmailThread(models.Model):
     staff = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="email_threads")
     email_thread = models.TextField()
-    customer_name = models.CharField(max_length=255)
+    customer_name = models.CharField(max_length=255, unique=True)
     last_updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        unique_together = ('staff', 'customer_name')
 
     def __str__(self):
         return f"Thread with {self.customer_name} by {self.staff.username}"
