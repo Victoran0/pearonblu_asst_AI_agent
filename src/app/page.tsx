2d578edd7172ba265d96896b96d9b313d6200925
Victoran0 <victoranosunkvic@gmail.com>
2025-01-05 23:13:58 +0100

    Updated todo comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,11 +29,7 @@ export default function Home() {
   );
 }
 
-// ADD THE TOGGLE DARK MODE AND ADMIN LOGIN FUNCTIONALITY
 // isStaff status to enter the /admin page
 // admin panel to create and destroy login credentials
-// only admin can create / delete a user for the application
 // create a /admin where the admin can login and do their work
-// request for a user's username and password on the home page before showing the chat page
-// if they are logged in (use session cookies), redirect to the chat page, else redirect to login page
 // add a newchat option to create a unique threadId, also we can save old chats by their threadId 
