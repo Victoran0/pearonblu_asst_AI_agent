45a2c13cddba1bd4d617df6ab9c89bd60323e3d9
Victoran0 <victoranosunkvic@gmail.com>
2025-01-06 04:02:56 +0100

    Remove non required imports

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,6 @@
 "use client"
 import React from "react";
-import { SessionProvider } from "next-auth/react";
-import { auth } from "@/auth";
 import ChatBody from "./chat-body";
-import { useSession } from "next-auth/react";
-import LoginPage from "../login/page";
 
 const Chat = () => {
 
