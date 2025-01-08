39432fc6022a9bf2a10426942b30c5472de189d2
Victoran0 <victoranosunkvic@gmail.com>
2025-01-08 03:00:52 +0100

    Changed header text size to 19.76px

--- src/components/asst-header.tsx
+++ src/components/asst-header.tsx
@@ -9,7 +9,7 @@ const AsstHeader = () => {
 
     return (
         <div className={cn("flex w-fit items-center colorGradText", theme === "light"?"bdBox":"")}>
-            <h1 className="pl-5 text-2xl py-2 cursor-pointer" >Pearon Blu Assistant</h1>
+            <h1 className="max-[700px]:text-[19.76px] pl-5 lg:text-2xl text-2xl py-2 cursor-pointer" >Pearon Blu Assistant</h1>
             <div className="w-8"></div>
             <div className="pr-2">
                 <ModeToggle />
