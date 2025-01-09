87f76f17cd9a0b05a726c6e8ec12598efb44c1de
Victoran0 <victoranosunkvic@gmail.com>
2025-01-09 21:07:37 +0100

    utilize PandS document from the database to initialize the Chroma DB

--- api/agentic_rag.py
+++ api/agentic_rag.py
@@ -1,19 +1,42 @@
 from langchain_chroma import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import TextLoader
+from langchain_core.documents import Document
+
+from datetime import datetime, timedelta
+import pytz
 from uuid import uuid4
 import os.path as path
 from dotenv import load_dotenv
 
+from .models import PandSDocument
+
+
 load_dotenv()
 
 
-def load_and_preprocess_doc():
+def get_doc_from_db():
+    doc = PandSDocument.objects.first()
+    return doc.document, doc
+
+
+def update_upsert_date(db_doc):
+    db_doc.last_upserted = str(
+        datetime.now(pytz.utc) + timedelta(seconds=2))
+    db_doc.save()
+    # print("last upserted: ", db_doc.last_upserted,
+    #   'last updated: ', db_doc.last_updated)
+
+
+def load_and_preprocess_doc(doc: str):
     # Load and Preprocess the document into chunks
-    text_loader_kwargs = {"autodetect_encoding": True}
-    loader = TextLoader(r'pricing_and_services.txt', **text_loader_kwargs)
-    document = loader.load()
+    document = [
+        Document(
+            page_content=doc,
+            metadata={"source": "database"},
+            id=1,
+        )
+    ]
     text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
         chunk_size=150, chunk_overlap=60)
     doc_splits = text_splitter.split_documents(document)
@@ -23,7 +46,8 @@ def load_and_preprocess_doc():
 
 
 def p_and_s_chrome_db():
-    doc_splits = load_and_preprocess_doc()
+    doc, db_doc = get_doc_from_db()
+    doc_splits = load_and_preprocess_doc(doc)
 
     # Define the root directory path and unique ids for each doc
     root_directory = path.dirname(
@@ -37,15 +61,29 @@ def p_and_s_chrome_db():
             model="models/text-embedding-004"),
         persist_directory=root_directory
     )
-    print("p and s vector store loaded successfully")
+    # print("p and s vector store initialized successfully")
 
     if vector_store.get()['ids'] == []:
+        # Add doc
         vector_store.add_documents(documents=doc_splits, ids=uuids)
-        print("documents added to the vector store")
-        print(vector_store.get()['ids'])
+        # update the last upserted date of the doc
+        update_upsert_date(db_doc)
+        # print("documents added to the vector store")
+        # print(vector_store.get()['ids'])
     else:
-        print("vector store have been initialized previously")
-        pass
+        if datetime.fromisoformat(str(db_doc.last_updated)) < datetime.fromisoformat(db_doc.last_upserted):
+            # print("doc was upserted recently")
+            pass
+        else:
+            print("doc was updated, lets Reset Chroma DB and upsert latest doc_splits")
+            # Reset the previous collection
+            vector_store.reset_collection()
+            # print("the collection is now Reset")
+            # Add the new doc data
+            vector_store.add_documents(documents=doc_splits, ids=uuids)
+            # update the last upserted date of the doc
+            update_upsert_date(db_doc)
+            # print("documents added to the vector store")
 
     return vector_store
 
@@ -53,9 +91,9 @@ def p_and_s_chrome_db():
 def query_p_and_s_chroma_db(query: str):
     vector_store = p_and_s_chrome_db()
 
-    print('\n\n-----similarity search-----\n\n')
+    # print('\n\n-----similarity search-----\n\n')
     joined_retrieved_docs = "\n\n".join(
         [i.page_content for i in vector_store.similarity_search(query, k=3)])
 
-    print(joined_retrieved_docs)
+    # print(joined_retrieved_docs)
     return joined_retrieved_docs
