780bab86814336ed643a744a0b6a6fdb668be9e5
Victoran0 <victoranosunkvic@gmail.com>
2025-01-10 01:15:50 +0100

    Updated footer Nav Links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-700">
@@ -13,30 +15,30 @@ export default function Footer() {
 
           {/* Navigation Links */}
           <div className="flex space-x-6">
-            <a
-              href="#about"
+            <Link
+              href="/chats"
               className="text-gray-600 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
             >
-              About
-            </a>
-            <a
-              href="#services"
+              Chats
+            </Link>
+            <Link
+              href="/rephrase"
               className="text-gray-600 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
             >
-              Services
-            </a>
-            <a
-              href="#blog"
+              Rephrase
+            </Link>
+            <Link
+              href="/doc"
               className="text-gray-600 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
             >
-              Blog
-            </a>
-            <a
-              href="#contact"
+              Doc
+            </Link>
+            <Link
+              href="/help"
               className="text-gray-600 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
             >
-              Contact
-            </a>
+              Help
+            </Link>
           </div>
         </div>
 
