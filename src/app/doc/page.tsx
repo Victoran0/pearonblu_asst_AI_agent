7511394e9139d3ee051af95adb85bc638a296e85
Victoran0 <victoranosunkvic@gmail.com>
2025-01-11 17:00:14 +0100

    Add spacing, separators and imporove the styling

--- src/app/doc/page.tsx
+++ src/app/doc/page.tsx
@@ -7,6 +7,7 @@ import EditDoc from './EditDoc'
 import { formattedResponse } from '../utils'
 import DOMPurify from 'dompurify'
 import { useSession } from 'next-auth/react'
+import { Separator } from '@/components/ui/separator'
 
 const Doc = () => {
     const {data: session} = useSession({required: true})
@@ -31,9 +32,11 @@ const Doc = () => {
 
     return (
         <div className="max-w-7xl max-h-fit min-h-[100vh] mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="h-10"></div>
+            <Separator />
             <div className="flex justify-between h-16 items-center">
                 <div className="flex-shrink-0">
-                <h1 className='text-xl font-bold text-gray-800 dark:text-white'>Pricing and Services Document</h1>
+                <h1 className='text-lg font-bold colorGradText'>Pricing and Services Document</h1>
                 </div>
             </div>
             <div className="h-4"></div>
@@ -61,7 +64,7 @@ const Doc = () => {
                         ) : (
                             <div className='grid w-full items-center'>
                                 <div 
-                                    className='bbBox2 text-sm p-6 h-[70vh] overflow-scroll'
+                                    className='bbBox2 text-sm p-6 h-[65vh] overflow-scroll'
                                     dangerouslySetInnerHTML={{
                                         __html: DOMPurify.sanitize(formattedResponse(doc) ?? "",
                                         {USE_PROFILES: {html: true}})
